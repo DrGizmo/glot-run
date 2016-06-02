328e366e6ec285422e6c0e920f3cc27eb8de9712
Javier Provecho Fernandez <javiertitan@gmail.com>
2016-06-03 01:29:31 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-export API_ENVIRONMENT="development"
-export API_HTTP_LISTEN_IP="0.0.0.0"
-export API_HTTP_LISTEN_PORT="8090"
-export DATA_PATH="/tmp/glot/data/"
-export LOG_PATH="/tmp/glot/log/"
-export BASE_URL="http://localhost:8090"
-export ADMIN_TOKEN="clumeterin"
-export DOCKER_API_URL="http://10.0.0.46:2375"
-export DOCKER_RUN_TIMEOUT="15"
-export MAX_OUTPUT_SIZE="500000"
+exec erl -noshell -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -s glot
 
-cd `dirname $0`
-exec erl -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -s glot
